0789e0263a05290aab3cc67c18620b50d15002ce
Smart Samuel <smartsamuel017@gmail.com>
2024-08-29 15:50:21 +0100

    Initial Commit
    Usinf Sqlserver without migration

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,209 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
+using test.Models;
+
+namespace test.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly string connectionString;
+        public ProductController(IConfiguration configuration)
+        {
+            connectionString = configuration["ConnectionStrings:SqlServerDb"] ?? "";
+        }
+
+        [HttpPost]
+        public IActionResult CreateProduct(ProductDto productDto)
+        {
+            try
+            {
+                using(var connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+
+                    string sql = "INSERT INTO products " +
+                        "(name, brand, category, price, description) VALUES " +
+                        "(@name, @brand, @category, @price, @description)";
+
+                    using (var command = new SqlCommand (sql, connection))
+                    {
+                        command.Parameters.AddWithValue("@name", productDto.Name);
+                        command.Parameters.AddWithValue("@brand", productDto.Brand);
+                        command.Parameters.AddWithValue("@category", productDto.Category);
+                        command.Parameters.AddWithValue("@price", productDto.Price);
+                        command.Parameters.AddWithValue("@description", productDto.Description);
+
+
+                        command.ExecuteNonQuery();
+                    }
+                }
+            }
+            catch(Exception ex)
+            {
+                ModelState.AddModelError("Product", "Sorry we caught an error");
+                return BadRequest(ModelState);
+            }
+
+            return Ok();
+        }
+
+        [HttpGet]
+        public IActionResult GetProduct()
+        {
+            List<Product> products = new List<Product>();
+
+            try
+            {
+                using (var connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+
+                    string sql = "SELECT * FROM products ";
+
+                    using (var command = new SqlCommand (sql, connection))
+                    {
+                        using (var reader = command.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                Product product = new Product();
+
+                                product.Id = reader.GetInt32(0);
+                                product.Name = reader.GetString(1);
+                                product.Brand = reader.GetString(2);
+                                product.Category = reader.GetString(3);
+                                product.Price = reader.GetDecimal(4);
+                                product.Description = reader.GetString(5);
+                                product.Created_at = reader.GetDateTime(6);
+
+                                products.Add(product);
+                            }
+                        }
+                    }
+                }
+
+            }
+
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("Product", "Sorry we caught an error");
+                return BadRequest(ModelState);
+            }
+
+            return Ok(products);
+        }
+
+        [HttpGet("(id)")]
+        public IActionResult GetProduct(int id)
+        {
+            Product product = new Product();
+
+            try
+            {
+                using (var connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+
+                    string sql = "SELECT * FROM products WHERE id = @id";
+
+                    using (var command = new SqlCommand (sql, connection))
+                    {
+                        command.Parameters.AddWithValue("@id", id);
+
+                        using (var reader = command.ExecuteReader())
+                        {
+                            if (reader.Read())
+                            {
+                                product.Id = reader.GetInt32(0);
+                                product.Name = reader.GetString(1);
+                                product.Brand = reader.GetString(2);
+                                product.Category = reader.GetString(3);
+                                product.Price = reader.GetDecimal(4);
+                                product.Description = reader.GetString(5);
+                                product.Created_at = reader.GetDateTime(6);
+                            }
+                            else
+                            {
+                                return NotFound();
+                            }
+                        }
+                    }
+
+                }
+            }
+
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("Product", "Sorry we caught an error");
+                return BadRequest(ModelState);
+            }
+                return Ok(product);
+        }
+        [HttpPut("(id")]
+        public IActionResult UpdateProduct(int id, ProductDto productDto)
+        {
+            try
+            {
+                using (var connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+
+                    string sql = "UPDATE products SET name =@name, brand = @brand, category=@category, " +
+                        "price =@price, description=@description WHERE id=@id";
+
+                    using (var command = new SqlCommand(sql, connection))
+                    {
+                        command.Parameters.AddWithValue("@name", productDto.Name);
+                        command.Parameters.AddWithValue("@brand", productDto.Brand);
+                        command.Parameters.AddWithValue("@category", productDto.Category);
+                        command.Parameters.AddWithValue("@price", productDto.Price);
+                        command.Parameters.AddWithValue("@description", productDto.Description);
+                        command.Parameters.AddWithValue("@id", id);
+
+                        command.ExecuteNonQuery();
+                    }
+                }
+            }
+
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("Product", "Sorry we caught an error");
+                return BadRequest(ModelState);
+            }
+
+            return Ok();
+        }
+        [HttpDelete("(id)")]
+        public IActionResult DeleteProduct(int id)
+        {
+            try
+            {
+                using (var connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+
+                    string sql = "DELETE FROM products WHERE id=@id";
+
+                    using (var command = new SqlCommand(sql, connection))
+                    {
+                        command.Parameters.AddWithValue("@id", id);
+
+                        command.ExecuteNonQuery();
+                    }
+                }
+            }
+
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("Product", "Sorry we caught an error");
+                return BadRequest(ModelState);
+            }
+
+            return Ok();
+        }
+    }
+}
---
+++ Models/Product.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace test.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = "";
+        public string Brand { get; set; } = "";
+        public string Category { get; set; } = "";
+        public decimal Price { get; set; }
+        public string Description { get; set; } = "";
+        public DateTime Created_at { get; set; }
+    }
+}
